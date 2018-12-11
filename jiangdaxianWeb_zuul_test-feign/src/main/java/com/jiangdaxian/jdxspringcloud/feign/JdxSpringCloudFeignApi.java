7afaf65507e09be675f5d699910e3e2012fea35d
江达贤 <jiangdaxian@ds.cn>
2018-12-11 16:22:35 +0800

    1

--- jiangdaxianWeb_zuul_test-feign/src/main/java/com/jiangdaxian/jdxspringcloud/feign/JdxSpringCloudFeignApi.java
+++ jiangdaxianWeb_zuul_test-feign/src/main/java/com/jiangdaxian/jdxspringcloud/feign/JdxSpringCloudFeignApi.java
@@ -1,7 +1,8 @@
 package com.jiangdaxian.jdxspringcloud.feign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import com.jiangdaxian.jdxspringcloud.api.JdxSpringCloudProviderApi;
+
+import com.jiangdaxian.jdxspringcloud.api5.JdxSpringCloudProviderApi;
 import com.jiangdaxian.jdxspringcloud.feign.fallback.JdxSpringCloudFeignFallback;
 
 @FeignClient(value = JdxSpringCloudProviderApi.SERVICE_ID, fallbackFactory=JdxSpringCloudFeignFallback.class)
