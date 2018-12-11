473f4ff823e901a030d18abd7987f58dd7f1ed97
江达贤 <jiangdaxian@ds.cn>
2018-12-11 17:13:40 +0800

    Log

--- jiangdaxianWeb_zuul_test-service/src/test/java/com/jiangdaxian/rabbitmq/RecieveService3344.java
+++ jiangdaxianWeb_zuul_test-service/src/test/java/com/jiangdaxian/rabbitmq/RecieveService3344.java
@@ -1,5 +1,7 @@
 package com.jiangdaxian.rabbitmq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 
@@ -8,12 +10,15 @@ import com.jiangdaxian.rabbitmq.source.MySink3344;
 
 @EnableBinding(MySink3344.class)
 public class RecieveService3344 {
+	private static final Logger LOG = LoggerFactory.getLogger(RecieveService3344.class);
 	@StreamListener("myOutputJdx33")
 	public void myOutputJdx33(Object payload) {
 		System.out.println("myOutputJdx33:" +System.currentTimeMillis()+"_" +payload);
+		LOG.error("myOutputJdx33:" +System.currentTimeMillis()+"_" +payload);
 	}
 	@StreamListener("myOutputJdx44")
 	public void myOutputJdx44(MyBean myBean) {
 		System.out.println("myOutputJdx44:" +System.currentTimeMillis()+"_" +myBean.getId() + "_" + myBean.getName());
+		LOG.error("myOutputJdx44:" +System.currentTimeMillis()+"_" +myBean.getId() + "_" + myBean.getName());
 	}
 }
